ea317700e57d9f793773bfb6bfa66c1e69f0263c
AdonayRH <abdonay2002@gmail.com>
2024-11-28 10:56:43 -0500

    login_done

--- gym_app/views.py
+++ gym_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth import login, authenticate, logout, get_user_model
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import UserRegistrationForm, UserLoginForm
@@ -26,16 +26,19 @@ def user_login(request):
         if form.is_valid():
             email = form.cleaned_data.get('email')
             password = form.cleaned_data.get('password')
-            user = authenticate(request, email=email, password=password)
-            if user is not None:
-                login(request, user)
-                messages.success(request, 'Session sussecful')
-                return redirect('home')
-            else:
-                messages.error(request, 'Email o password error')
+            try:
+                user = get_user_model().objects.get(email=email)
+                if user.check_password(password):
+                    login(request, user)
+                    messages.success(request, 'Session successful')
+                    return redirect('home')
+                else:
+                    messages.error(request, 'Invalid password')
+            except get_user_model().DoesNotExist:
+                messages.error(request, 'Invalid email address')
     else:
         form = UserLoginForm()
-    return render(request, 'login.html', {'form':form})
+    return render(request, 'login.html', {'form': form})
     
 # home
 @login_required
